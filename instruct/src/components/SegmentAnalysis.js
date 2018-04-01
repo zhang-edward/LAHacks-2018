6ec9df733f7f93c6a157f6e882930319f2992410
Edward Zhang <edwardz123@gmail.com>
2018-03-31 23:12:51 -0700

    Added python script runner

---
+++ instruct/src/components/SegmentAnalysis.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SegmentAnalysis = (props) => {
+	if (props.segment.imagePath == undefined) {
+		return (
+			<div>
+				Click on a segment of text to see its analysis
+			</div>
+		);
+	}
+	return (
+		<div>
+			<img 
+				alt="screen cap"
+				style={{ width: 200, height: 200 }} 
+				src={process.env.PUBLIC_URL + '/frames/' + props.segment.imagePath + '.jpg'}/>
+			<p>Attentiveness: {props.segment.attentiveness}</p>
+		</div>
+	);
+}
+
+export default SegmentAnalysis;
